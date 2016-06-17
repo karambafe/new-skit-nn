f03c08deaeee4d88f22c9b05c099a4f4f925250a
Vitaly Blokhin <karambafe@yahoo.com>
2016-06-17 20:45:18 +0300

    add jquery for nav menu

--- js/main.js
+++ js/main.js
@@ -99,7 +99,14 @@ informationBoxThird.onclick = function() {
     }
 }
 
-
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
 
 
 
